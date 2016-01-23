94e1c40c40050da1b5f00fe393ad4ca4f0d484f7
dasscheman <dasscheman@gmail.com>
2016-01-23 22:55:13 +0100

    Setting up the codeception tests

--- tests/travis/setup/setup_apache.sh
+++ tests/travis/setup/setup_apache.sh
@@ -4,7 +4,7 @@ sudo apt-get -qq install -y --force-yes apache2 libapache2-mod-php5 php5-curl ph
 sudo a2enmod rewrite
 
 ## Setting the home directory for localhost.
-sudo sed -i -e "s,/var/www,/home/travis/build/dasscheman/Hike3_development,g" /etc/apache2/sites-available/default
+sudo sed -i -e "s,/var/www,/home/travis/build/dasscheman/hike3_development,g" /etc/apache2/sites-available/default
 sudo sed -i -e "s,AllowOverride[ ]None,AllowOverride All,g" /etc/apache2/sites-available/default
 
 sudo /etc/init.d/apache2 restart
