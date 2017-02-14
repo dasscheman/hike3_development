16f8e144fd12643d2dd66c36ecd015d4b2f8631c
Daan Asscheman <dasscheman@gmail.com>
2017-02-14 17:13:31 +0100

    Delete db.php

--- config/db.php
+++
@@ -1,8 +0,0 @@
-<?php
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=hike3',
-    'username' => 'hike3',
-    'password' => '5HHEMhUSfcnFXJFh',
-    'charset' => 'utf8',
-];
