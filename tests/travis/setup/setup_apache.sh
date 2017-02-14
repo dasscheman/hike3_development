8ae6d727ae868d897c190020b52defc908af77cd
dasscheman <dasscheman@gmail.com>
2017-02-14 21:36:59 +0100

    setup travis

--- tests/travis/setup/setup_apache.sh
+++ tests/travis/setup/setup_apache.sh
@@ -1,7 +1,8 @@
 echo "Install and setup apache"
-# sudo apt-get update > /dev/null
+sudo apt-get update > /dev/null
+sudo apt-get -qq install -y --force-yes apache2 libapache2-mod-fastcgi
 # sudo apt-get -qq install -y --force-yes apache2 libapache2-mod-php5 php5-curl php5-intl php5-gd php5-idn php-pear php5-imagick php5-imap php5-mcrypt php5-memcache php5-ming php5-ps php5-pspell php5-recode php5-snmp php5-sqlite php5-tidy php5-xmlrpc php5-xsl php5-mysql uopz
-# sudo a2enmod rewrite
+sudo a2enmod rewrite
 
 ## Setting the home directory for localhost.
 # convert_selenium_to_phpunit.sh assumes that the files are in a folder with this pattern: */svn/*/trunk
