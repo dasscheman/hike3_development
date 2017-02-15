1abe31b6b8004311399815a317ae0cc1ec89564a
dasscheman <dasscheman@gmail.com>
2017-02-15 09:01:51 +0100

    setup travis

--- tests/codeception/config/unit.php
+++ tests/codeception/config/unit.php
@@ -4,8 +4,8 @@
  */
 return yii\helpers\ArrayHelper::merge(
     require(__DIR__ . '/../../../config/web.php'),
-    require(__DIR__ . '/config.php'),
-    [
-
-    ]
+    require(__DIR__ . '/config.php')
+    // [
+    //
+    // ]
 );
