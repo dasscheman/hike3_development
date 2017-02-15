70888560999edb82bea6250bfafbfcb6143e59b7
dasscheman <dasscheman@gmail.com>
2017-02-15 23:06:59 +0100

    setup travis

--- tests/codeception/acceptance/AboutCept.php
+++ tests/codeception/acceptance/AboutCept.php
@@ -5,8 +5,6 @@ use tests\codeception\_pages\AboutPage;
 /* @var $scenario Codeception\Scenario */
 
 $I = new AcceptanceTester($scenario);
-$test = $I->grabFromCurrentUrl();
-$I->wantTo($test);
-// $I->wantTo('ensure that about works');
+$I->wantTo('ensure that about works');
 AboutPage::openBy($I);
 $I->see('About', 'h1');
