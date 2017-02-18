cedc86127f4a1bd2a152e6866a501cd31eb586fd
dasscheman <dasscheman@gmail.com>
2017-02-18 22:38:43 +0100

    error in logincept

--- tests/codeception/functional/LoginCept.php
+++ tests/codeception/functional/LoginCept.php
@@ -1,24 +1,4 @@
 <?php
-
-use yii\helpers\Url as Url;
-
-class ContactCest
-{
-    public function _before(\AcceptanceTester $I)
-    {
-        $I->amOnPage(Url::toRoute('/site/contact'));
-    }
-
-    public function contactPageWorks(AcceptanceTester $I)
-    {
-        $I->wantTo('ensure that contact page works');
-        $I->see('Contact', 'h1');
-    }
-
-    public function contactFormCanBeSubmitted(AcceptanceTester $I)
-    {
-        $I->amGoingTo('submit contact form with correct data');
-        $I->fillField('#contactform-name', 'tester');<?php
 class LoginFormCest
 {
     public function _before(\FunctionalTester $I)
@@ -74,18 +54,5 @@ class LoginFormCest
         ]);
         $I->see('Logout (admin)');
         $I->dontSeeElement('form#login-form');
-    }
-}
-        $I->fillField('#contactform-email', 'tester@example.com');
-        $I->fillField('#contactform-subject', 'test subject');
-        $I->fillField('#contactform-body', 'test content');
-        $I->fillField('#contactform-verifycode', 'testme');
-
-        $I->click('contact-button');
-
-        $I->wait(2); // wait for button to be clicked
-
-        $I->dontSeeElement('#contact-form');
-        $I->see('Thank you for contacting us. We will respond to you as soon as possible.');
     }
 }
