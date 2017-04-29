d027fea4af6b2daec1ea9ae6dbcd35db42a4727e
dasscheman <dasscheman@gmail.com>
2017-04-29 11:33:24 +0200

    Create profile feed.

--- config/params.php
+++ config/params.php
@@ -3,4 +3,9 @@
 return [
     'adminEmail' => 'admin@biologenkantoor.nl',
     'event_images_url' => 'uploads/event_images/',
+    'adsense' => [
+        'client'  => 'ca-pub-5643906969935410',
+        'slot'    => '1234567890',
+        'enabled' => false,
+    ],
 ];
