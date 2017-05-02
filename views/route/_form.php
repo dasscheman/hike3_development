b0503edcfc5542035bc66f6bfa2fd560f009c8ab
dasscheman <dasscheman@gmail.com>
2017-05-02 16:16:50 +0200

    Issue with adding route http://bugs.biologenkantoor.nl/project/1/issue/3

--- views/route/_form.php
+++ views/route/_form.php
@@ -13,8 +13,11 @@ use yii\widgets\ActiveForm;
     <?php $form = ActiveForm::begin([
         'action' => $model->isNewRecord ? ['route/create'] : ['route/update', 'id' => $model->route_ID]]); ?>
 
-    <?= $form->field($model, 'route_name')->textInput(['maxlength' => true]) ?>
-
+    <?php
+        echo $form->field($model, 'route_name')->textInput(['maxlength' => true]);
+        echo $form->field($model, 'event_ID')->hiddenInput(['value'=> $model->event_ID])->label(false);
+        echo $form->field($model, 'day_date')->hiddenInput(['value'=> $model->day_date])->label(false);
+    ?>
     <div class="form-route">
         <?= Html::submitButton($model->isNewRecord ? Yii::t('app', 'Create') : Yii::t('app', 'Update'), ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']);
 
