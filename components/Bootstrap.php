a0745e3566b5193a217a53ce345bd619d0187793
dasscheman <dasscheman@gmail.com>
2017-05-17 23:03:29 +0200

    Added kiwilogo params

--- components/Bootstrap.php
+++ components/Bootstrap.php
@@ -11,5 +11,7 @@ class Bootstrap implements BootstrapInterface
         // Here you can refer to Application object through $app variable
         $app->params['event_images_path'] = $app->basePath . '/web/uploads/event_images/';
         $app->params['qr_code_path'] = $app->basePath . '/web/qr/';
+        $app->params['kiwilogo'] = $app->basePath . '/web/images/kiwilogo.jpg';
+        // $app->params['qr_code_path'] = $app->basePath . '/web/qr/';
     }
 }
