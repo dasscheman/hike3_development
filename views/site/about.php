da20053fb93a1d09bc9465b893fd71f136d87899
dasscheman <dasscheman@gmail.com>
2017-08-25 11:42:17 +0200

    Added manuals.

--- views/site/about.php
+++ views/site/about.php
@@ -19,4 +19,28 @@ $this->title = 'About';
     <p>
         Illustrations by <a align="center" href="https://vecteezy.com">Vecteezy!</a>
     </p>
+
+    <h2><?= Html::encode('change log') ?></h2>
+    <h3><?= Html::encode('3.1') ?></h3>
+    <p>
+        Added Export functionality. You can export a complete hike.
+    </p>
+    <p>
+        Added Time Trail. This is comparible with the silent stations. 
+        But the qr code of a time trail have a time restriction. The player have 
+        to scan them within a certain time. Time trail can run over several day's.
+    </p>
+    <p>
+        Added manuals. This is not complete and we are still experimenting with different formats.
+    </p>
+    <p>
+        Added change log.
+    </p>
+    <p>
+        Several small bugs.
+    </p>
+    <h3><?= Html::encode('3.0') ?></h3>
+    <p>
+        Complete rewrite in Yii2
+    </p>
 </div>
---
+++ views/site/quick-start.php
@@ -0,0 +1,94 @@
+<?php
+
+/* @var $this yii\web\View */
+
+use yii\helpers\Html;
+
+$this->title = 'My Yii Application';
+?>
+<div class="site-index">
+
+    <div class="jumbotron">
+        <h1><?php echo Yii::t('app', 'Quick Start') ?></h1>
+    </div>
+
+    <div class="body-content">
+        <div class="row">
+            <div class="col-lg-4">
+                <h2>Iedereen: Hoe doe je mee aan een hike.</h2>
+
+                <p>Beschrijving hoe je aan een hike mee kan doen als deelnemer
+                    of als organisatie. Let op, als je organisatie bent en je
+                    wilt een hike opzetten, dan moet je de prezi 'Hoe zet je een
+                    hike op' bekijken.</p>
+
+            </div>
+            <div class="col-lg-8">
+                <p>
+                    <iframe id="iframe_container" frameborder="0" webkitallowfullscreen="" mozallowfullscreen="" allowfullscreen="" width="775" height="600" src="https://prezi.com/embed/jt-zbfbaujnd/?bgcolor=ffffff&amp;lock_to_path=0&amp;autoplay=0&amp;autohide_ctrls=0&amp;landing_data=bHVZZmNaNDBIWnNjdEVENDRhZDFNZGNIUE43MHdLNWpsdFJLb2ZHanI5dVFJbGVCRW0wMVUwM3ZTaW51ZkZ4ZHFBPT0&amp;landing_sign=aTmukMWboBXNgQu5afTC567SHn9btTZSyYAB2ZV-Eq0"></iframe>
+                </p>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-lg-4">
+                <h2>Deelnemer: Hoe gebruik je kiwi.run tijdens een hike.</h2>
+
+                <p>Korte omschrijving hoe je kiwi.run gebruikt tijdens de hike.</p>
+
+            </div>
+            <div class="col-lg-8">
+                <p>
+                    <iframe width="775" height="600" src="http://www.powtoon.com/embed/fY9Bc3DHsjq/" frameborder="0"></iframe>
+                </p>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-lg-4">
+                <h2>Organisatie: Hoe zet je een hike op.</h2>
+
+                <p>Korte omschrijving hoe je een hike moet opzetten als organisatie.
+                Deze stap kan je al doen als je ook nog bezig bent met het uitzetten van de hike.</p>
+
+            </div>
+            <div class="col-lg-8">
+                <h2>Stap 1</h2>
+                <p>
+                    <iframe id="iframe_container" frameborder="0" webkitallowfullscreen="" mozallowfullscreen="" allowfullscreen="" width="775" height="600" src="https://prezi.com/embed/5olrjmvjv09-/?bgcolor=ffffff&amp;lock_to_path=0&amp;autoplay=0&amp;autohide_ctrls=0&amp;landing_data=bHVZZmNaNDBIWnNjdEVENDRhZDFNZGNIUE43MHdLNWpsdFJLb2ZHanI5Z1ZMV2R0YzdNZmlnZnplWjBTYjNlU2J3PT0&amp;landing_sign=XBmaDkpIS07yDcJGd6CmLtQQUgsT9cfB1yQKQQUFlgc"></iframe>
+                </p>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-lg-4">
+                <h2>Organisatie: Hoe voer je de route in.</h2>
+
+                <p>Korte omschrijving hoe je een hike moet opzetten als organisatie.
+                Deze stap kan je al doen als je ook nog bezig bent met het uitzetten van de hike.</p>
+
+            </div>
+            <div class="col-lg-8">
+                <h2>Stap 1</h2>
+                <p>
+                    <iframe id="iframe_container" frameborder="0" webkitallowfullscreen="" mozallowfullscreen="" allowfullscreen="" width="775" height="600" src="tutorial/kiwirun_route_maken.sozi.html" seamless></iframe>
+                </p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-4">
+                <h2>Organisatie: Hoe start je de hike.</h2>
+
+                <p>Korte omschrijving hoe je een hike moet opzetten als organisatie.
+                Deze stap kan je al doen als je ook nog bezig bent met het uitzetten van de hike.</p>
+
+            </div>
+            <div class="col-lg-8">
+                <h2>Stap 1</h2>
+                <p>
+                    <iframe id="iframe_container" frameborder="0" webkitallowfullscreen="" mozallowfullscreen="" allowfullscreen="" width="775" height="600" src="https://prezi.com/embed/5olrjmvjv09-/?bgcolor=ffffff&amp;lock_to_path=0&amp;autoplay=0&amp;autohide_ctrls=0&amp;landing_data=bHVZZmNaNDBIWnNjdEVENDRhZDFNZGNIUE43MHdLNWpsdFJLb2ZHanI5Z1ZMV2R0YzdNZmlnZnplWjBTYjNlU2J3PT0&amp;landing_sign=XBmaDkpIS07yDcJGd6CmLtQQUgsT9cfB1yQKQQUFlgc"></iframe>
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
