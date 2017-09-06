e28b80511e904bd20584c2f08ac2029a14650799
dasscheman <dasscheman@gmail.com>
2017-09-06 19:09:13 +0200

    Changed some config settings.

--- config/web-test.php
+++ config/web-test.php
@@ -95,11 +95,13 @@ if (YII_ENV_DEV) {
     $config['bootstrap'][] = 'debug';
     $config['modules']['debug'] = [
         'class' => 'yii\debug\Module',
+        'allowedIPs' => ['127.0.0.1', '::1', '145.133.104.158'],
     ];
 
     $config['bootstrap'][] = 'gii';
     $config['modules']['gii'] = [
         'class' => 'yii\gii\Module',
+        'allowedIPs' => ['127.0.0.1', '::1', '145.133.104.158'],
     ];
 }
 
