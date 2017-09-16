d43dd0d9de9ffe69738eefa1d0c594342e38ab63
dasscheman <dasscheman@gmail.com>
2017-09-16 21:06:50 +0200

    Added email in params.

--- config/params.php
+++ config/params.php
@@ -2,8 +2,8 @@
 
 return [
     'admin_email' => 'admin@kiwi.run',
-    'contact_email' => 'contact@kiwi.run',
-    'noreply_email' => 'noreply@kiwi.run',
+    'contact_email' => 'kiwi@biologenkantoor.nl',
+    'noreply_email' => 'kiwi@biologenkantoor.nl',
     'event_images_url' => 'uploads/event_images/',
     'event_import_url' => 'uploads/import/',
 ];
