bf4213cc0e95cbd23761fda44b0d35e41ca1f0ca
dasscheman <dasscheman@gmail.com>
2017-10-02 22:15:46 +0200

    Use debug and logging from certain ip's

--- web/index.php
+++ web/index.php
@@ -1,8 +1,11 @@
 <?php
 
-// comment out the following two lines when deployed to production
-defined('YII_DEBUG') or define('YII_DEBUG', true);
-defined('YII_ENV') or define('YII_ENV', 'dev');
+// NOTE: Make sure this file is not accessible when deployed to production
+if (in_array(@$_SERVER['REMOTE_ADDR'], ['127.0.0.1', '::1', '145.133.104.158'])) {
+    // comment out the following two lines when deployed to production
+    defined('YII_DEBUG') or define('YII_DEBUG', true);
+    defined('YII_ENV') or define('YII_ENV', 'dev');
+}
 
 require(__DIR__ . '/../vendor/autoload.php');
 require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
