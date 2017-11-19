9c4d065dc4ca024f859630d9ac5783a9cbddcbb4
dasscheman <dasscheman@gmail.com>
2017-11-19 10:40:47 +0100

    added new bonuspunten permissions

---
+++ rbac/BonuspuntenCreateRule.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace app\rbac;
+
+use Yii;
+use yii\rbac\Rule;
+use app\models\DeelnemersEvent;
+use app\models\EventNames;
+
+/**
+ * Checks if authorID matches user passed via params
+ */
+class BonuspuntenCreateRule extends Rule
+{
+    /**
+     * @param string|int $user the user ID.
+     * @param Item $item the role or permission that this rule is associated with
+     * @param array $params parameters passed to ManagerInterface::checkAccess().
+     * @return bool a value indicating whether the rule permits the role or permission it is associated with.
+     */
+    public function execute($user, $item, $params)
+    {
+        if (Yii::$app->user->identity->deelnemersEventsByUserID->event->status >= EventNames::STATUS_introductie and
+            Yii::$app->user->identity->deelnemersEventsByUserID->rol == DeelnemersEvent::ROL_organisatie) {
+            return TRUE;
+        }
+        return FALSE;
+    }
+}
