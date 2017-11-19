32b4adbbe5449950a4bc94972dce21ab2a86e1e6
dasscheman <dasscheman@gmail.com>
2017-11-19 10:41:54 +0100

    added new deelnemersevent permissions

---
+++ rbac/DeelnemersEventUpdateRule.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace app\rbac;
+
+use Yii;
+use yii\rbac\Rule;
+use app\models\DeelnemersEvent;
+
+/**
+ * Checks if authorID matches user passed via params
+ */
+class DeelnemersEventUpdateRule extends Rule
+{
+    /**
+     * @param string|int $user the user ID.
+     * @param Item $item the role or permission that this rule is associated with
+     * @param array $params parameters passed to ManagerInterface::checkAccess().
+     * @return bool a value indicating whether the rule permits the role or permission it is associated with.
+     */
+    public function execute($user, $item, $params)
+    {
+        if (Yii::$app->user->identity->deelnemersEventsByUserID->rol == DeelnemersEvent::ROL_organisatie) {
+            return TRUE;
+        }
+
+        return FALSE;
+    }
+}
