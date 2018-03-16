c88a2de7a33b8f2f99928a0aef9ca47789471f7c
dasscheman <dasscheman@gmail.com>
2018-03-16 10:25:05 +0100

    fix migrate issue, fix binnenkomst en vertrek field in postenPassage.

--- migrations/m170930_133206_fix_data_issues_before_migrate.php
+++ migrations/m170930_133206_fix_data_issues_before_migrate.php
@@ -5,6 +5,7 @@ use yii\db\Migration;
 use app\models\EventNames;
 use app\models\Route;
 use app\models\Posten;
+use app\models\PostPassage;
 
 /**
  * Class m171104_143306_update_rbac_data
@@ -41,6 +42,24 @@ class m170930_133206_fix_data_issues_before_migrate extends Migration
         foreach ($posten as $post) {
             $post->date = NULL;
             $post->save(FALSE);
+        }
+
+        $postPassages = PostPassage::find()
+            ->where(['binnenkomst' => '0000-00-00'])
+            ->all();
+
+        foreach ($postPassages as $postPassage) {
+            $postPassage->binnenkomst = NULL;
+            $postPassage->save(FALSE);
+        }
+
+        $postPassages = PostPassage::find()
+            ->where(['vertrek' => '0000-00-00'])
+            ->all();
+
+        foreach ($postPassages as $postPassage) {
+            $postPassage->vertrek = NULL;
+            $postPassage->save(FALSE);
         }
     }
 
