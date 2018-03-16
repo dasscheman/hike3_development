c4f5c33f57af46d8a55fe903b7c8130b8f8fb4ba
dasscheman <dasscheman@gmail.com>
2018-03-16 09:44:07 +0100

    fix migrate issue

--- migrations/m170930_133206_fix_data_issues_before_migrate.php
+++ migrations/m170930_133206_fix_data_issues_before_migrate.php
@@ -3,7 +3,7 @@
 //use Yii;
 use yii\db\Migration;
 use app\models\EventNames;
-
+use app\models\Route;
 
 /**
  * Class m171104_143306_update_rbac_data
@@ -22,6 +22,15 @@ class m170930_133206_fix_data_issues_before_migrate extends Migration
         foreach ($events as $event) {
             $event->active_day = NULL;
             $event->save();
+        }
+
+        $routes = Route::find()
+            ->where(['day_date' => '0000-00-00'])
+            ->all();
+
+        foreach ($routes as $route) {
+            $route->day_date = NULL;
+            $route->save();
         }
     }
 
