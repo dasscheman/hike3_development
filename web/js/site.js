5433157bb6657b015404d62a62b803b5b595344e
dasscheman <dasscheman@gmail.com>
2018-06-01 21:19:48 +0200

    fine tuning

--- web/js/site.js
+++ web/js/site.js
@@ -40,7 +40,7 @@ document.getElementById("map-click").addEventListener("click", setCookie);
 
 window.onload = function() {
     var tracker = new Tracker();
-    tracker.setSavingInterval(5000);
+    tracker.setSavingInterval(300000);
     tracker.setGPSConfiguration({
         enableHighAccuracy: true,
         timeout: 10000,
