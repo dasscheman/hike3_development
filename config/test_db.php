cf90258ff458d260ae1e59a931057f36d86db811
dasscheman <dasscheman@gmail.com>
2018-07-18 23:02:26 +0200

    Fine tuning testing

--- config/test_db.php
+++ config/test_db.php
@@ -1,5 +1,5 @@
 <?php
-$db = require __DIR__ . '/db.php.dist';
+$db = require __DIR__ . '/db.php';
 // test database! Important not to run tests on production or development databases
 $db['dsn'] = 'mysql:host=localhost;dbname=hikeapptest';
 
