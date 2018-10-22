916e5ae0748d5fa780abd7b00a88b88513d6fbc6
dasscheman <dasscheman@gmail.com>
2018-10-22 11:55:55 +0200

    added ignore file

---
+++ assets/LeafletFullscreenAsset.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ *
+ */
+
+namespace app\assets;
+
+use yii\web\AssetBundle;
+
+/**
+ * Leaflet Fullscreen application asset bundle.
+ *
+ * @author Qiang Xue <qiang.xue@gmail.com>
+ * @since 2.0
+ */
+class LeafletFullscreenAsset extends AssetBundle
+{
+    public $sourcePath = '@bower/leaflet.fullscreen';
+    public $css = [
+        'Control.FullScreen.css'
+    ];
+    public $js = [
+        'Control.FullScreen.js',
+    ];
+}
